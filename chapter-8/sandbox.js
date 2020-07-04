2a6c4b2f1ca2fb4f3b37e7df466639e53d573628
kolaczyn <kolaczyn@protonmail.com>
2020-07-04 20:05:48 +0200

    nothing special

---
+++ chapter-8/sandbox.js
@@ -0,0 +1,44 @@
+// function canYouSpotTheProblem(){
+//   for(counter = 0; counter<10; counter++){
+//     console.log("Happy happy");
+//   }
+// }
+
+// canYouSpotTheProblem();
+
+// "use strict";
+// function Person(name) {
+// }
+
+// let fedinand = Person("Ferdinand"); // oops
+// console.log(name);
+
+// (ViallegeState, Array) -> {direction: string, memory: Array}
+// function goalOrientedRobot(state, memory){
+
+// }
+
+// function test(label, body){
+//   if(!body()) console.log(`Failed: ${label}`);
+// }
+
+// test("convert Latin text to uppercase", ()=>{
+//   return "hello".toUpperCase() == 'HELLO';
+// })
+
+// function numberToString(n, base=10){
+//   let result = "", sign = "";
+//   if(n<0){
+//     sign = '-';
+//     n=-n
+//   }
+//   do{
+//     result= String(n%base)+result;
+//     n=Math.floor(n/base)
+//     debugger
+//     }while(n>0)
+//   return sign+result;
+// }
+
+// console.log(numberToString(13,10)
+
